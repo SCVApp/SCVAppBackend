0fe13dfb01e506babffa6f89db08f91a0f8fa0f2
Urban Krepel <urban.kreppel@gmail.com>
2022-02-12 13:03:01 +0100

    Update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -128,6 +128,7 @@ export class UserController{
                 selectedSchool.razred = razred.displayName
             }else{
                 let idSole = data.value.find(e=>e.mailEnabled == false && e.securityEnabled == true && e.groupTypes.length == 0 && Object.keys(SchoolsInfo).includes(e.displayName))
+                console.log(data.value)
                 if(idSole){
                     selectedSchool.id = idSole
                 }
