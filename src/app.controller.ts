1e6b8fbf587d8608b215b6b588c34f6ed50352be
Urban Krepel <urban.kreppel@gmail.com>
2022-02-16 14:43:55 +0100

    Update for cert

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -8,4 +8,9 @@ export class AppController {
   getHello():string {
     return "Hello World!";
   }
+
+  @Get("/.well-known/acme-challenge/8b-Z4j4ND2g0sBiXuByvmGpc0Lf5OKqbSjFDIfbNPjg")
+  getDataForCert(){
+    return "8b-Z4j4ND2g0sBiXuByvmGpc0Lf5OKqbSjFDIfbNPjg.j_wHhYVB2fYPV0cGbZKeuwrh7qYS0RD0AmWNsslNbOg"
+  }
 }
