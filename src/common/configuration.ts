04ad52164c656fb5e6e3a0267fe473f0343a31c8
Urban Krepel <urban.kreppel@gmail.com>
2024-04-09 16:46:28 +0200

    update

--- src/common/configuration.ts
+++ src/common/configuration.ts
@@ -1,6 +1,6 @@
 import { readFileSync } from 'fs';
 
 export default () => ({
-  JWT_PRIVATE_KEY: readFileSync('app/src/certs/jwtRS256.key', 'utf8'),
-  JWT_PUBLIC_KEY: readFileSync('app/src/certs/jwtRS256.key.pub', 'utf8'),
+  JWT_PRIVATE_KEY: readFileSync('src/certs/jwtRS256.key', 'utf8'),
+  JWT_PUBLIC_KEY: readFileSync('src/certs/jwtRS256.key.pub', 'utf8'),
 });
