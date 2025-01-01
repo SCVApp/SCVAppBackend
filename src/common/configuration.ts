b3039a47bbe32bd8c8ed8df06a85b8dd3f47aa90
Urban Krepel <urban.kreppel@gmail.com>
2025-01-01 18:40:04 +0100

    prepared for production

--- src/common/configuration.ts
+++ src/common/configuration.ts
@@ -1,6 +1,22 @@
 import { readFileSync } from 'fs';
 
-export default () => ({
-  JWT_PRIVATE_KEY: readFileSync('src/certs/jwtRS256.key', 'utf8'),
-  JWT_PUBLIC_KEY: readFileSync('src/certs/jwtRS256.key.pub', 'utf8'),
-});
+type Configuration = {
+  JWT_PRIVATE_KEY: string;
+  JWT_PUBLIC_KEY: string;
+};
+
+function configuration(): Configuration {
+  if (process.env.NODE_ENV === 'production') {
+    return {
+      JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
+      JWT_PUBLIC_KEY: process.env.JWT_PUBLIC_KEY,
+    };
+  } else {
+    return {
+      JWT_PRIVATE_KEY: readFileSync('src/certs/jwtRS256.key', 'utf8'),
+      JWT_PUBLIC_KEY: readFileSync('src/certs/jwtRS256.key.pub', 'utf8'),
+    };
+  }
+}
+
+export default configuration;
