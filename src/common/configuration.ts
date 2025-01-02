ade49dc137d5564f90bc817c5c8f4d8941ddefe1
Urban Krepel <urban.kreppel@gmail.com>
2025-01-02 20:39:09 +0100

    bug fix

--- src/common/configuration.ts
+++ src/common/configuration.ts
@@ -1,21 +1,6 @@
 import { readFileSync } from 'fs';
 
-type Configuration = {
-  JWT_PRIVATE_KEY: string;
-  JWT_PUBLIC_KEY: string;
-};
-
-function configuration(): Configuration {
-  if (process.env.NODE_ENV === 'production') {
-    return {
-      JWT_PRIVATE_KEY: process.env.JWT_PRIVATE_KEY,
-      JWT_PUBLIC_KEY: process.env.JWT_PUBLIC_KEY,
-    };
-  }
-  return {
-    JWT_PRIVATE_KEY: readFileSync('src/certs/jwtRS256.key', 'utf8'),
-    JWT_PUBLIC_KEY: readFileSync('src/certs/jwtRS256.key.pub', 'utf8'),
-  };
-}
-
-export default configuration;
+export default () => ({
+  JWT_PRIVATE_KEY: readFileSync('src/certs/jwtRS256.key', 'utf8'),
+  JWT_PUBLIC_KEY: readFileSync('src/certs/jwtRS256.key.pub', 'utf8'),
+});
